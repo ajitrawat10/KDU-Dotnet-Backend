503b9e9c4561fd3f0818074588627a759acf8783
Ajit Singh Rawat <47390463+ajitrawat10@users.noreply.github.com>
2022-07-07 21:03:30 +0530

    Update Program.cs

--- Session02-Homework/Program.cs
+++ Session02-Homework/Program.cs
@@ -1,1 +1,53 @@
 
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace SerializeDeserializeXMLData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string xmlString =  SerializeObjectToXmlString();
+            Console.WriteLine(xmlString);
+
+
+            DeserializeXmlStringToObject(xmlString);
+        }
+
+        // Serializing  Method 
+        private static string SerializeObjectToXmlString()
+        {
+            var member = new Member
+            {
+                Name = "Pradeep",
+                Email = "pradepp23@gmail.com",
+                Age = 23,
+                JoiningDate = DateTime.Now,
+            };
+
+            var xmlSerializer = new XmlSerializer(typeof(Member));
+            using (var writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, member); ;
+                var xmlContent = writer.ToString();
+                return xmlContent;
+            }
+         }
+
+        //Deserializing Method
+        private static void DeserializeXmlStringToObject(string xmlString)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Member));
+            using (var reader = new StringReader(xmlString))
+            {
+                var member = (Member)xmlSerializer.Deserialize(reader);
+
+
+                Console.WriteLine(member);
+            }
+        }
+
+    }
+}
