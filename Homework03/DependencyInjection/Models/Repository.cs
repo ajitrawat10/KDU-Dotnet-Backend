6b6b0abddea0eb3edde3dd773b25c1651f205ff2
Ajit Singh Rawat <47390463+ajitrawat10@users.noreply.github.com>
2022-07-08 21:34:28 +0530

    Add files via upload

---
+++ Homework03/DependencyInjection/Models/Repository.cs
@@ -0,0 +1,21 @@
+﻿namespace DependencyInjection.Models
+{
+    public class Repository : IRepository
+    {
+        private Dictionary<string, Product> products;
+        public Repository()
+        {
+            products = new Dictionary<string, Product>();
+            new List<Product> {
+                new Product { Name = "Shoes", Price = 99M },
+                new Product { Name = "Shirts", Price = 29.99M },
+                new Product { Name = "Pants", Price = 40.5M }
+            }.ForEach(p => AddProduct(p));
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+        public Product this[string name] => products[name];
+        public void AddProduct(Product product) => products[product.Name] = product;
+        public void DeleteProduct(Product product) => products.Remove(product.Name);
+    }
+}
