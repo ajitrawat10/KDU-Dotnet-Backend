20cef56576caf1eb9147ce23c59ea83cd4f3a4af
Ajit Singh Rawat <47390463+ajitrawat10@users.noreply.github.com>
2022-07-14 23:39:18 +0530

    Add files via upload

---
+++ Homework-5/Models/NorthwindContext.Customizations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFCoreScaffolding.Models
+{
+    public partial class NorthwindContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Territory>().
+        }
+    }
+}
---
+++ Homework-5/Program.cs
@@ -0,0 +1,74 @@
+﻿using EFCoreScaffolding.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                     .SetBasePath(Directory.GetCurrentDirectory())
+                     .AddJsonFile("appsettings.json");
+
+            var config = configuration.Build();
+
+            var connectionString = config.GetConnectionString("NorthwindDbConnection");
+
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                                .UseLazyLoadingProxies()
+                                .UseSqlServer(connectionString)
+                                .Options;
+
+            var dbContext = new NorthwindContext(options);
+
+
+           
+
+            //Query - 1
+           
+
+            var Id = 2;
+            var employeeList = dbContext.Employees.Where(e => e.EmployeeId == Id).Include(e => e.Territories).ToList();
+
+            foreach (var employee in employeeList)
+            {
+                foreach (var emp in employee.Territories)
+                {
+                    Console.WriteLine(emp.TerritoryDescription);
+                }
+            }
+
+
+            // Query -2
+
+
+
+
+
+            // Query - 3
+            var prod = dbContext.Invoices.Select(x => new
+            {
+                productname = x.ProductName,
+                ship_con = x.ShipCountry
+            }).ToList();
+
+            foreach(var x in prod)
+            {
+                Console.WriteLine(x.productname + " " + x.ship_con);
+            }
+
+
+            //Query-4
+
+
+
+
+
+
+
+
+        }
+    }
+}
