b8092735297504a2d9d8fa87e91b328fee08dcef
Ajit Singh Rawat <47390463+ajitrawat10@users.noreply.github.com>
2022-07-22 23:48:36 +0530

    Add files via upload

---
+++ Homework-8/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Homework_8.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Homework_8.Services;
+
+namespace Homework_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+       
+
+        private readonly UserRepository userRepository ;
+        public AuthController(UserRepository userRepository)
+        {
+            this.userRepository= userRepository;
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public string Get(string username,string password)
+        {
+            var token = userRepository.GenerateToken(username, password);
+
+            return (string)token;
+        }
+
+        [HttpPost]
+        public bool Get(string token)
+        {
+            bool isValid = (bool)userRepository.ValidateToken(token, out var validatedToken, out var principal);
+            if (isValid)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+
+
+            }
+
+        }
+    }
+}
